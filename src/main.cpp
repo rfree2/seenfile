91e824962ed899455804e20ca8e799dc5b61f6c5
rfree2 <rfree@op.pl>
2014-05-14 16:27:34 +0200

    Working on test case

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,24 @@
 INJECT_OT_COMMON_USING_NAMESPACE_COMMON_1
 
 
+typedef string cHash; // ""
+typedef string cTestimony;
+typedef list<cTestimony> cTestimonyList;
+
 
 class cKnowledge {
-	public:
-		
+	private:
+		map< cHash , cTestimonyList >
+
+	public:		
+		cKnowledge();
 };
+
+cKnowledge::cKnowledge() {
+}
 
 
 int main(int argc, const char **argv) {
-	cout << "Hello sector 0-0-0. Resistance is fuitile." << endl;
-
-	vector<string> vec;
-	vec.push_back("Locutus");
-	vec.push_back("Picard");
-	vec.push_back("1of4");
-
-	for (auto s : vec) cout << s << endl;
 
 }
 
