d98177e993940c6289d56ea0fe20a053ae0f812f
rfree2 <rfree@op.pl>
2014-05-14 16:06:07 +0200

    Hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 
+#include "lib_common1.hpp"
+
+INJECT_OT_COMMON_USING_NAMESPACE_COMMON_1
+
+
 
 class cKnowledge {
 	public:
 		
 };
 
+
 int main(int argc, const char **argv) {
+	cout << "Hello sector 0-0-0. Resistance is fuitile." << endl;
+
+	vector<string> vec;
+	vec.push_back("Locutus");
+	vec.push_back("Picard");
+	vec.push_back("1of4");
+
+	for (auto s : vec) cout << s << endl;
 
 }
 
